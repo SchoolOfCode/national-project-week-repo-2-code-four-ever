b54256812f14b07747445c9ccce23543d08dab27
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-01-19 16:38:42 +0000

    Removed link

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Button from "../Button";
 
 const Card = ({weeknumber, daynumber }) => {
@@ -15,9 +15,6 @@ const Card = ({weeknumber, daynumber }) => {
     <div id="card-id">
       <h2>Day 1</h2>
       <Button action={handleClick}/>
-      {/* <Link to={"/cardviewer"} state={{ msg: 'bella ragazzi' }}>
-        <Button />
-      </Link> */}
     </div>
   );
 };
